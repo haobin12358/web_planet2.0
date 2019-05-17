91f99ff2a386c55f973e48ad053e6d29d009e5d7
ericwww <a965799128@gmail.com>
2019-05-17 13:45:03 +0800

    Create magicBox.vue

---
+++ src/views/buyer/activity/magicBox.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="w-magicbox">
+		<div class="w-mb-img">
+
+		</div>
+		<div class="w-mb-price">
+
+		</div>
+		<div class="w-mb-product-info">
+			<div class="w-mb-product-title">
+
+			</div>
+			<div class="w-mb-product-prop">
+
+			</div>
+		</div>
+		<div class="w-mb-format">
+
+		</div>
+		<div class="w-mb-rule">
+
+		</div>
+
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+
+	},
+	methods:{
+
+	},
+	created(){
+		
+	}
+	
+}
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>
